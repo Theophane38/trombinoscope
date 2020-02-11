e8e8b7b89eed3cfbc1a8ac32f8697937b59555b2
FlorianLC <flo0388@free.fr>
2020-02-11 16:11:57 +0100

    adding people lvl 1

--- src/App.js
+++ src/App.js
@@ -1,17 +1,41 @@
 import React from 'react';
-import logo from './logo.svg';
+import people from './assets/datas/people.json';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-<<<<<<< HEAD
-      <p>Test 3</p>
-=======
-      <p>Test 2</p>
->>>>>>> 3444d7483efd4e3edb993fe91eb97105f8b26946
-    </div>
-  );
+class Main extends React.Component {
+
+  constructor() {
+    super()
+
+    this.state = {
+
+      allData: people,
+    }
+  }
+
+  peopleLvl1(data) {
+    let tabLvl1 = []
+    for (let i = 0; i < data.length; i++) {
+      if (data[i].level === 1) {
+        tabLvl1.push (
+          <div className='level1'>
+            <p>{data[i].lastname} {data[i].firstname}</p>
+            <p>{data[i].location}</p>
+          </div>
+        )
+      }
+    }
+    return (tabLvl1)
+  }
+
+  render() {
+    return (
+      <div className='Main'>
+        {this.peopleLvl1(this.state.allData)}
+      </div>
+      )
+  }
+
 }
 
-export default App;
+export default Main;
