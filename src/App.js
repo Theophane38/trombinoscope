d8e0efe5b8ae9b793320eae29b0d875585911753
FlorianLC <flo0388@free.fr>
2020-02-11 17:16:04 +0100

    adding people lvl 2 and 3

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ class Main extends React.Component {
     for (let i = 0; i < data.length; i++) {
       if (data[i].level === 1) {
         tabLvl1.push (
-          <div className='level1'>
+          <div className= 'PeopleLvl1'>
             <p>{data[i].lastname} {data[i].firstname}</p>
             <p>{data[i].location}</p>
           </div>
@@ -28,10 +28,50 @@ class Main extends React.Component {
     return (tabLvl1)
   }
 
+  peopleLvl2(data) {
+    let tabLvl2 = []
+    for (let i = 0; i < data.length; i++) {
+      if (data[i].level === 2) {
+        tabLvl2.push (
+          <div className= 'PeopleLvl2'>
+            <p>{data[i].lastname} {data[i].firstname}</p>
+            <p>{data[i].location}</p>
+          </div>   
+        )
+      }
+    }
+    return (tabLvl2)
+  }
+
+  peopleLvl3(data) {
+    let tabLvl3 = []
+    for (let i = 0; i < data.length; i++) {
+      if (data[i].level === 3) {
+        tabLvl3.push (
+          <div className= 'PeopleLvl3'>
+            <p>{data[i].lastname} {data[i].firstname}</p>
+            <p>{data[i].location}</p>
+          </div>   
+        )
+      }
+    }
+    return (tabLvl3)
+  }
+
   render() {
     return (
       <div className='Main'>
-        {this.peopleLvl1(this.state.allData)}
+        <div className='Level1'>
+          {this.peopleLvl1(this.state.allData)}
+        </div>
+        <p><font size="10">&darr;</font></p>
+        <div className='Level2'>
+          {this.peopleLvl2(this.state.allData)}
+        </div>
+        <p><font size="10">&darr;</font></p>
+        <div className='Level3'>
+          {this.peopleLvl3(this.state.allData)}
+        </div>
       </div>
       )
   }
