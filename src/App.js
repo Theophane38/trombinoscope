db0dc78fbac69eff151ff815ffe385148d541363
FlorianLC <flo0388@free.fr>
2020-02-12 11:42:52 +0100

    adding services containers

--- src/App.js
+++ src/App.js
@@ -10,7 +10,24 @@ class Main extends React.Component {
     this.state = {
 
       allData: people,
+      allServices: this.getServices(people)
     }
+  }
+
+  getServices(data) {
+    let service = {}
+    for (let i = 0; i < data.length; i++) {
+      if (typeof service[data[i].service] === 'undefined') {
+        service[data[i].service] = []
+      }
+      service[data[i].service].push({
+        firstname: data[i].firstname,
+        lastname: data[i].lastname,
+        level: data[i].level,
+      })
+    }
+    console.log(service)
+    return (service)
   }
 
   peopleLvl1(data) {
@@ -18,7 +35,7 @@ class Main extends React.Component {
     for (let i = 0; i < data.length; i++) {
       if (data[i].level === 1) {
         tabLvl1.push (
-          <div className= 'PeopleLvl1'>
+          <div className= 'manger'>
             <p>{data[i].lastname} {data[i].firstname}</p>
             <p>{data[i].location}</p>
           </div>
@@ -28,50 +45,55 @@ class Main extends React.Component {
     return (tabLvl1)
   }
 
-  peopleLvl2(data) {
-    let tabLvl2 = []
-    for (let i = 0; i < data.length; i++) {
-      if (data[i].level === 2) {
-        tabLvl2.push (
-          <div className= 'PeopleLvl2'>
-            <p>{data[i].lastname} {data[i].firstname}</p>
-            <p>{data[i].location}</p>
-          </div>   
-        )
+  trombinoscope() {
+    let services = this.state.allServices
+    let tabTrombino = []
+    for (let i in services){
+      console.log(i)
+      let peopleLevel2 = []
+      let peopleLevel3 = []
+      for (let j = 0; j < services[i].length; j++){
+        console.log(services[i][j].level)
+        if (services[i][j].level === 2){
+          peopleLevel2.push(
+            <div className= 'people'>
+              <p>{services[i][j].lastname} {services[i][j].firstname}</p>
+              <p>{services[i][j].location}</p>
+            </div>
+          )
+        } else if (services[i][j].level === 3){
+          console.log('test')
+          peopleLevel3.push(
+            <div className= 'people'>
+              <p>{services[i][j].lastname} {services[i][j].firstname}</p>
+              <p>{services[i][j].location}</p>
+            </div>
+          )
+        }
       }
+      console.log(peopleLevel3)
+      tabTrombino.push(
+        <div className={i + ' serviceContainer'}>
+          <div className='level2'>
+            {peopleLevel2}
+          </div>
+          <div className='level3'>
+            {peopleLevel3}
+          </div>
+        </div>
+      )
     }
-    return (tabLvl2)
+    return tabTrombino
   }
 
-  peopleLvl3(data) {
-    let tabLvl3 = []
-    for (let i = 0; i < data.length; i++) {
-      if (data[i].level === 3) {
-        tabLvl3.push (
-          <div className= 'PeopleLvl3'>
-            <p>{data[i].lastname} {data[i].firstname}</p>
-            <p>{data[i].location}</p>
-          </div>   
-        )
-      }
-    }
-    return (tabLvl3)
-  }
 
   render() {
     return (
       <div className='Main'>
         <div className='Level1'>
           {this.peopleLvl1(this.state.allData)}
-        </div>
-        <p><font size="10">&darr;</font></p>
-        <div className='Level2'>
-          {this.peopleLvl2(this.state.allData)}
-        </div>
-        <p><font size="10">&darr;</font></p>
-        <div className='Level3'>
-          {this.peopleLvl3(this.state.allData)}
-        </div>
+        </div> 
+        {this.trombinoscope()}
       </div>
       )
   }
